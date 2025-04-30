389b80ab0b6d2307ee0e5adbca726ad8a239c01b
Jan Paul Batrina <jpmbatrina01@gmail.com>
2025-04-30 14:02:08 +0800

    feat: add transaction history to account details

--- src/app/account-details/accounts-details.component.ts
+++ src/app/account-details/accounts-details.component.ts
@@ -3,10 +3,13 @@ import { Account } from '../types/account';
 import { AccountService, AccountsPage } from '../account.service';
 import { AccountCardComponent } from '../account-card/account-card.component';
 import { ActivatedRoute } from '@angular/router';
+import { TransactionService, TransactionsPage } from '../transaction.service';
+import { Transaction } from '../types/transaction';
+import { TransactionCardComponent } from "../transaction-card.component";
 
 @Component({
   selector: 'app-account-details',
-  imports: [AccountCardComponent],
+  imports: [AccountCardComponent, TransactionCardComponent],
   template: `
     <h1 class="mb-4 text-xl md:text-2xl">
       <div>
@@ -15,13 +18,29 @@ import { ActivatedRoute } from '@angular/router';
     </h1>
     <div>
         <app-account-card [account]="account" [isClickable]=true/>
+ 
+        <div class="m-5 p-5">
+          <h2 class="ml-2 text-xl font-extrabold">Transaction History</h2>
+          @if (this.transactions.length > 0) {
+            @for (transaction of this.transactions; 
+                track "account-" + this.account.accountId + '-transaction-' + transaction.transactionId
+            ) {
+              <app-transaction-card [transaction]="transaction" />
+            }
+          } @else {
+            <h3 class="ml-2 text-l">There are no transactions for this account</h3>
+          }
+        </div>
     </div>
   `
 })
 export class AccountDetailsComponent implements OnInit{
   // accountId = input.required<number>();
   accountsService = inject(AccountService);
+  transactionsService = inject(TransactionService);
+
   account!: Account;
+  transactions!: Transaction[];
 
   constructor(private route: ActivatedRoute) {}
 
@@ -34,6 +53,15 @@ export class AccountDetailsComponent implements OnInit{
       alert(errMsg);
     } else {
       this.account = accountGet;
+    }
+
+    const transactionsGet: TransactionsPage | null = await this.transactionsService.getTransactionsForAccount(accountId);
+    if (!transactionsGet) {
+      const errMsg = `Fetch for transactions of user with id ${accountId} failed`;
+      console.error(errMsg);
+      alert(errMsg);
+    } else {
+      this.transactions = transactionsGet.transactions;
     }
   };
 }
