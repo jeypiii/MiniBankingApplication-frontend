f33da4f85a0bf032cfa8ea2a4a69ce4514ffc7d4
Jan Paul Batrina <jpmbatrina01@gmail.com>
2025-04-30 10:31:37 +0800

    fix: make whole account card clickable

--- src/app/account-card/account-card.component.ts
+++ src/app/account-card/account-card.component.ts
@@ -5,16 +5,16 @@ import { Account } from '../types/account';
   selector: 'app-account-card',
   imports: [],
   template: `
-      <div class="rounded-xl bg-gray-50 p-2 shadow-sm min-w-max cursor-pointer">
+      <div class="rounded-xl bg-gray-50 p-2 shadow-sm min-w-max cursor-pointer"
+        (click)="goToAccountDetails()"
+      >
       <div class="flex p-4">
         <!-- {Icon ? <Icon class="h-5 w-5 text-gray-700" /> : null} -->
         <h3 class="ml-2 text-sm font-mono font-extrabold">Account No. {{ accountNumber() }}</h3>
         <!-- <h5 class="block ml-2 text-sm font-small ml-6">Id: {{ this.account.accountId }}</h5> -->
       </div>
   
-      <div class="rounded-xl bg-white px-4 py-8 text-left text-sxs shrink-0 min-w-max"
-        (click)="goToAccountDetails()"
-      >
+      <div class="rounded-xl bg-white px-4 py-8 text-left text-sxs shrink-0 min-w-max">
           @for(item of displayDetailItems(); track 'account-' + this.account().accountId + '-' + item[0]) {
                 <p class="font-semibold inline">{{item[0]}}: </p>
                 <p class="font-normal inline px-3 min-w-max">{{item[1]}}</p>
