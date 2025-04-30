58a9572223d78e7c32a0a837d3403180b5a2fabf
Jan Paul Batrina <jpmbatrina01@gmail.com>
2025-04-30 15:37:08 +0800

    feat: add check balance and fund transfer buttons (non-functional)

--- src/app/account-details/accounts-details.component.ts
+++ src/app/account-details/accounts-details.component.ts
@@ -7,9 +7,13 @@ import { TransactionService, TransactionsPage } from '../transaction.service';
 import { Transaction } from '../types/transaction';
 import { TransactionCardComponent } from "../transaction-card.component";
 
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroBanknotes, heroPaperAirplane} from '@ng-icons/heroicons/outline'
+
 @Component({
   selector: 'app-account-details',
-  imports: [AccountCardComponent, TransactionCardComponent],
+  imports: [AccountCardComponent, TransactionCardComponent, NgIcon],
+  providers: [provideIcons({ heroBanknotes, heroPaperAirplane})],
   template: `
     <h1 class="mb-4 text-xl md:text-2xl">
       <div>
@@ -18,7 +22,17 @@ import { TransactionCardComponent } from "../transaction-card.component";
     </h1>
     <div>
         <app-account-card [account]="account" [isClickable]=false/>
- 
+
+        <div class="flex flex-row">
+          <button type="submit" class="m-5 mt-4 w-full flex-1 h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50">
+            <ng-icon name="heroBanknotes" class="ml-auto h-5 w-5 text-gray-50" />
+            Check Balance 
+          </button>
+          <button type="submit" class="m-5 mt-4 w-full flex-1 h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50">
+            <ng-icon name="heroPaperAirplane" class="ml-auto h-5 w-5 text-gray-50" />
+            New Fund Transfer 
+          </button>
+        </div>
         <div class="m-5 p-5">
           <h2 class="ml-2 text-xl font-extrabold">Transaction History</h2>
           @if (this.transactions.length > 0) {
